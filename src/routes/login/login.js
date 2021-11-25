6a98979add1d97bb2e18d08784e630d480b80b2d
AlanVH <alanvhz.09@gmail.com>
2021-11-25 13:37:16 -0600

    Login UI

--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -6,20 +6,32 @@ export const Login = (props) => {
         <div className="container-fluid p-0">
             <div className="row">
                 <div className="col">
-                    <div className="half-side-container flex-column" id="left-side">
-                        <h1 className="py-3">Header</h1>
-                        <div className="mb-3">
-                            <input type="email" className="form-control" id="username-input" placeholder="Usuario" />
+                    <div className="half-side-container d-flex flex-column" id="left-side">
+                        <div id="form-container" className="d-flex flex-column justify-content-center">
+                            <h1 className="py-5 text-center">Ingresa a <a id="name-link" href="">Centro de Optica</a></h1>
+                            <div className="mb-4 d-flex align-items-center">
+                                <img id="username-icon" src="/img/user.png" />
+                                <input type="email" className="form-control" id="username-input" placeholder="Usuario" />
+                            </div>
+                            <div className="mb-4 d-flex align-items-center">
+                                <img id="password-icon" src="/img/password.png" />
+                                <input type="password" className="form-control" id="password-input" placeholder="Contraseña" />
+                            </div>
+                            <div className="mb-4 p-2 d-flex align-items-center justify-content-between">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch" id="show-password-switch" />
+                                <label class="form-check-label" for="flexSwitchCheckDefault">Mostrar contraseña</label>
+                            </div>
+                                <button type="button" class="btn" id="login-btn">Ingresar</button>
+                            </div>
                         </div>
-                        <div className="mb-3">
-                            <input type="password" className="form-control" id="password-input" placeholder="Contraseña" />
-                        </div>
-                        <button type="button" class="btn" id="login-btn">Ingresar</button>
                     </div>
                 </div>
                 <div className="col">
                     <div className="half-side-container" id="right-side">
-                     
+                        <div id="logo-container" className="d-flex flex-column justify-content-center">
+                            <h1 className="d-flex justify-content-center">L O G O</h1>
+                        </div>
                     </div>
                 </div>
             </div>
